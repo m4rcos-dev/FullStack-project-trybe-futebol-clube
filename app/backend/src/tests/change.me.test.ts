d42ee4477bc3671278e7629ef43caa22b2cc9afa
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-02 20:43:09 -0300

    :test_tube: test: start tests tdd

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,15 +4,56 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import User from '../database/models/UserModel';
 
 import { Response } from 'superagent';
+import { after, before } from 'node:test';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Testando API Trybe Fuebol Clube', () => {
+  describe('Testa endpoint /login', () => {
+      // TRIPLE AAA
+      // ARRANGE - arranjar / arrumar
+      const token = {
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc" // Aqui deve ser o token gerado pelo backend.
+      }
+
+      const resultFindOne = {
+        id: 1,
+        username: 'anyuser',
+        role: 'anyrole',
+        email: 'any@email.com'
+      }
+
+      const bodyLogin = {
+        email: "any@anyemail.com",
+        password: "anypassword"
+      }
+
+      before(async () => {
+        sinon
+          .stub(User, 'findOne')
+          .resolves(resultFindOne as User)
+      });
+
+      after(async () => {
+        (User.findOne as sinon.SinonStub).restore();
+      });
+
+    it('Retorna um token ao se logar corretamente', async () => {
+      // ACT - agir / executar
+      const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(bodyLogin)
+      // ASSERT - verificar
+      expect(httpResponse.status).to.be.equal(200);
+      expect(httpResponse.body).to.be.deep.equal(token);
+    })
+  });
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -39,7 +80,7 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
 });
