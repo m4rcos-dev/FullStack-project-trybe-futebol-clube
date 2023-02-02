84fc06d65053a55987044d6887b1d99cc4924a6c
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-02 20:44:24 -0300

    :sparkles: feat: create UserModel

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,43 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+    unique: true,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+    unique: true,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'users',
+});
+
+export default User;
