77a3fe3a1f5e45e94b206b132ff4b4172e8355f2
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-02 21:54:03 -0300

    :sparkles: feat: create LoginInterface

---
+++ app/backend/src/services/LoginService.ts
@@ -0,0 +1,9 @@
+// import sequelize from '../database/models';
+// import UserModel from '../database/models/UserModel';
+
+// export default class LoginService {
+//   public async sigInUser(email: string) {
+//     const user = await sequelize.models.UserModel.findOne({ where: { email } });
+//     if (!user) return { status: 400, message: 'Invalid' };
+//   }
+// }
