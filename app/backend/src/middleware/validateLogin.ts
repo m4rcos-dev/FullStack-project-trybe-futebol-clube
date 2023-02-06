280d587a48b484880c1bce6bac81b691cfe78f5f
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-06 15:04:20 -0300

    :sparkles: feat: create middleware validateLoginWhitourPassword

--- app/backend/src/middleware/validateLogin.ts
+++ app/backend/src/middleware/validateLogin.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 const validateLogin = (req: Request, res: Response, next: NextFunction): Response | void => {
-  const { email } = req.body;
-  if (!email) return res.status(400).json({ message: 'All fields must be filled' });
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
   next();
 };
 
