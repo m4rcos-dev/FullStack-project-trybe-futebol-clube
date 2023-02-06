297ef501acde0fe1d63c45d4cd7526ae35bdf4a5
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-06 15:45:07 -0300

    :sparkles: feat: create validation password with bcrypt

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcryptjs';
 import { IUserModel } from '../interfaces/UserInterface';
 import { ILogin, ILoginResult, ILoginService } from '../interfaces/LoginInterface';
 import tokenGenerate from '../utils/tokenGenerate';
@@ -11,7 +12,13 @@ export default class LoginService implements ILoginService {
 
   async assinInUser(user: ILogin): Promise<ILoginResult> {
     const result = await this._userModel.findByEmail(user.email);
-    if (!result) return { status: 401, message: 'Incorrect email or password' };
+    if (!result || !bcrypt
+      .compareSync(
+        user
+          .password,
+        result
+          .password,
+      )) { return { status: 401, message: 'Incorrect email or password' }; }
     const token = tokenGenerate(result);
     return { status: 200, token };
   }
