48aba2ca84a69410476fd8ff5a709761b7703c1b
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-07 15:39:37 -0300

    :sparkles: feat: create Teams Model

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,28 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+    unique: true,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
