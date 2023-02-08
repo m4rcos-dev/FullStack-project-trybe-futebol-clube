110614a695c117545ee96a11a454cf318f893521
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-08 10:49:40 -0300

    :sparkles: feat: create /teams/:id route

--- app/backend/src/routes/teamsRouter.ts
+++ app/backend/src/routes/teamsRouter.ts
@@ -3,6 +3,7 @@ import TeamsService from '../services/TeamsService';
 import TeamsController from '../controller/TeamsController';
 import TeamsModel from '../models/TeamsModel';
 import TeamsSequelizeRepository from '../repository/sequelize/TeamsSequelizeRepository';
+import { resultTeam } from '../tests/mocks/teams';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ const teamsService = new TeamsService(teamsModel);
 const teamsController = new TeamsController(teamsService);
 
 router.get('/', (req, res) => teamsController.getAll(req, res));
+router.get('/:id', (req, res) => res.status(200).json(resultTeam));
 
 export default router;
