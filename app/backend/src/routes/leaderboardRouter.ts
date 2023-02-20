74bd3b685511c2fd0b6f3ae34af859de3f68e59f
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-20 16:15:23 -0300

    :sparkles: feat: create leaderboardRouter with endpoint /leaderboard

--- app/backend/src/routes/leaderboardRouter.ts
+++ app/backend/src/routes/leaderboardRouter.ts
@@ -3,6 +3,7 @@ import LeaderboardService from '../services/LeaderboardService';
 import LeaderboardController from '../controller/LeaderboardController';
 import LeaderboardModel from '../models/LeaderboardModel';
 import LeaderboardSequelizeRepository from '../repository/sequelize/LeaderboardSequelizeRepository';
+import { resultLeaderboards } from '../tests/mocks/leaderboards';
 
 const router = Router();
 
@@ -13,5 +14,6 @@ const leaderboardController = new LeaderboardController(leaderboardService);
 
 router.get('/home', (req, res) => leaderboardController.getAllHomeTeams(req, res));
 router.get('/away', (req, res) => leaderboardController.getAllAwayTeams(req, res));
+router.get('/', (req, res) => res.status(200).json(resultLeaderboards));
 
 export default router;
