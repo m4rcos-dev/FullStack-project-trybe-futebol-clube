543b11f050a37892b5df1e1fb6eb7af0b6953455
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-20 15:01:57 -0300

    :test_tube: test: endpoint /leaderboards/away

--- app/backend/src/tests/04_leaderboards.test.ts
+++ app/backend/src/tests/04_leaderboards.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Matches from '../database/models/Matches';
-import { resultLeaderboardsBd } from './mocks/leaderboards';
+import { resultLeaderboardsBd as resultLeaderboardsBdHome } from './mocks/leaderboards';
 import leaderboardHomeGenerate from '../utils/leaderboardHomeGenerate';
 
 
@@ -20,22 +20,38 @@ describe('Testa enpoint /leaderboards/home', () => {
   beforeEach(async () => {
     sinon
       .stub(Matches, 'findAll')
-      .resolves(resultLeaderboardsBd as unknown as Matches[])
+      .resolves(resultLeaderboardsBdHome as unknown as Matches[])
+    sinon
+      .stub(Matches, 'findAll')
+      .resolves(resultLeaderboardsBdAway as unknown as Matches[])
   });
 
   afterEach(async () => {
     (Matches.findAll as sinon.SinonStub).restore();
   });
 
-  const resultSortedLeaderboard = leaderboardHomeGenerate(resultLeaderboardsBd)
+  const resultSortedHome = leaderboardHomeGenerate(resultLeaderboardsBdHome);
+  const resultSortedAway = leaderboardHomeGenerate(resultLeaderboardsBdAway);
 
-  it('Retorna status 200 e um json contendo as leaderboards', async () => {
+  it('Retorna status 200 e um json contendo as leaderboards home', async () => {
     // ACT - agir / executar
     const httpResponse = await chai
       .request(app)
       .get('/leaderboard/home')
     // ASSERT - verificar
     expect(httpResponse.status).to.equal(200);
-    expect(httpResponse.body).to.be.deep.equal(resultSortedLeaderboard);
+    expect(httpResponse.body).to.be.deep.equal(resultSortedHome);
   });
+
+  describe('Testa enpoint /leaderboards/away', () => {
+    it('Retorna status 200 e um json contendo as leaderboards away', async () => {
+      // ACT - agir / executar
+      const httpResponse = await chai
+        .request(app)
+        .get('/leaderboard/away')
+      // ASSERT - verificar
+      expect(httpResponse.status).to.equal(200);
+      expect(httpResponse.body).to.be.deep.equal(resultSortedAway);
+    });
+  })
 });
