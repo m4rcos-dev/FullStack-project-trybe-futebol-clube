a007c214a89d2f7da3b811d37cbe3bcf1e51b18b
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-21 19:32:28 -0300

    :recycle: refactor: utils leaderboardAwayGenerate

--- app/backend/src/utils/leaderboardAwayGenerate.ts
+++ app/backend/src/utils/leaderboardAwayGenerate.ts
@@ -1,126 +1,40 @@
-import { ILeaderboardTeams, IMatchesAwayTeams } from '../interfaces/LeaderboardInterface';
+import { ILeaderboardTeams, IMatchesTeam } from '../interfaces/LeaderboardInterface';
+import { arrayUniqueTeams,
+  calc1AwayTeams,
+  calc2AwayTeams } from './leaderboardGenerate';
 import sortLeaderboard from './sortLeaderboard';
 
-const arrayUniqueTeams = (teams: IMatchesAwayTeams) => {
-  const arrayAllTeams = Object.values(teams).map((match) => match.awayTeam?.teamName);
-  const uniqueTeams = arrayAllTeams.filter((team, index, self) =>
-    index === self.findIndex((t) => (
-      t === team
-    )));
-  return uniqueTeams;
+const createLeaderboard = (teams: IMatchesTeam, c: string) => {
+  const { totalPoints, totalVictories,
+    totalDraws, totalLosses, efficiency } = calc1AwayTeams(teams, c);
+  const { goalsFavor, goalsOwn, goalsBalance } = calc2AwayTeams(teams, c);
+  return {
+    totalPoints,
+    totalGames: teams.length,
+    totalVictories,
+    totalDraws,
+    totalLosses,
+    goalsFavor,
+    goalsOwn,
+    goalsBalance,
+    efficiency,
+  };
 };
 
-const totalGames = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalGames = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2].awayTeam?.teamName) currentTeam += 1;
-    }
-    arrayAllTotalGames.push(currentTeam);
-  }
-  return arrayAllTotalGames;
-};
-
-const totalPoints = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalPoints = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2].awayTeam?.teamName
-      && teams[i2].awayTeamGoals > teams[i2].homeTeamGoals) currentTeam += 3;
-      if (arrayUniqueTeams(teams)[i] === teams[i2].awayTeam?.teamName
-      && teams[i2].awayTeamGoals === teams[i2].homeTeamGoals) currentTeam += 1;
-    }
-    arrayAllTotalPoints.push(currentTeam);
-  }
-  return arrayAllTotalPoints;
-};
-
-const totalVictories = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalVictories = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2].awayTeam?.teamName
-      && teams[i2].awayTeamGoals > teams[i2].homeTeamGoals) currentTeam += 1;
-    }
-    arrayAllTotalVictories.push(currentTeam);
-  }
-  return arrayAllTotalVictories;
-};
-
-const totalDraws = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalDraws = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2].awayTeam?.teamName
-      && teams[i2].awayTeamGoals === teams[i2].homeTeamGoals) currentTeam += 1;
-    }
-    arrayAllTotalDraws.push(currentTeam);
-  }
-  return arrayAllTotalDraws;
-};
-
-const totalLosses = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalLosses = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2].awayTeam?.teamName
-      && teams[i2].awayTeamGoals < teams[i2].homeTeamGoals) currentTeam += 1;
-    }
-    arrayAllTotalLosses.push(currentTeam);
-  }
-  return arrayAllTotalLosses;
-};
-
-const goalsFavor = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalFavor = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2]
-        .awayTeam?.teamName) currentTeam += teams[i2].awayTeamGoals;
-    }
-    arrayAllTotalFavor.push(currentTeam);
-  }
-  return arrayAllTotalFavor;
-};
-
-const goalsOwn = (teams: IMatchesAwayTeams) => {
-  const arrayAllTotalOwn = [];
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
-    let currentTeam = 0;
-    for (let i2 = 0; i2 < teams.length; i2 += 1) {
-      if (arrayUniqueTeams(teams)[i] === teams[i2]
-        .awayTeam?.teamName) currentTeam += teams[i2].homeTeamGoals;
-    }
-    arrayAllTotalOwn.push(currentTeam);
-  }
-  return arrayAllTotalOwn;
-};
-
-export default function leaderboardAwayGenerate(teams: IMatchesAwayTeams): ILeaderboardTeams[] {
-  const result = [];
-
-  for (let i = 0; i < arrayUniqueTeams(teams).length; i += 1) {
+const leaderboardHomeGenerate = (teams: IMatchesTeam) => {
+  const result: ILeaderboardTeams[] = [];
+  arrayUniqueTeams(teams, 'away').forEach((team) => {
     let currentTeam = {};
+    const filterTeam = Object.values(teams)
+      .filter((teamFilter) => teamFilter.homeTeam?.teamName === team
+      || teamFilter.awayTeam?.teamName === team);
     currentTeam = {
-      name: arrayUniqueTeams(teams)[i],
-      totalPoints: totalPoints(teams)[i],
-      totalGames: totalGames(teams)[i],
-      totalVictories: totalVictories(teams)[i],
-      totalDraws: totalDraws(teams)[i],
-      totalLosses: totalLosses(teams)[i],
-      goalsFavor: goalsFavor(teams)[i],
-      goalsOwn: goalsOwn(teams)[i],
-      goalsBalance: goalsFavor(teams)[i] - goalsOwn(teams)[i],
-      efficiency: Number(((totalPoints(teams)[i] / (totalGames(teams)[i] * 3)) * 100).toFixed(2)),
+      name: team,
+      ...createLeaderboard(filterTeam, team as string),
     };
     result.push(currentTeam);
-  }
-
+  });
   return sortLeaderboard(result as unknown as ILeaderboardTeams);
-}
+};
+
+export default leaderboardHomeGenerate;
