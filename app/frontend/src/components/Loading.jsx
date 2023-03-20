3e1713af34d5e18d763a066df402fbbbe3bd8a4d
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-20 16:50:03 -0300

    :package: build: add lottiefiles

--- app/frontend/src/components/Loading.jsx
+++ app/frontend/src/components/Loading.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Loading = () => (
   <div>
-    Loading...
+   <Player
+    autoplay
+    loop
+    src="https://assets4.lottiefiles.com/packages/lf20_xFpiNt.json"
+    style={{ height: '300px', width: '300px', marginTop: '270px'}}
+   />
   </div>
 );
 
