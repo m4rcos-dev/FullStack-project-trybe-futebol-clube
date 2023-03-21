8a924d675625b70b4a073e6bd9243d645ba4c218
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-21 00:23:16 -0300

    :recycle: refactor: add config in deploy frontent

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: `https://trybefutebolclube.bohr.io/:${process.env.REACT_APP_API_PORT}`,
 });
 
 export const setToken = (token) => {
