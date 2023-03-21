cdf3b3b52b7f05cad78e72fb54320d7135318688
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-21 10:57:36 -0300

    :recycle: refactor: add index bckend test

--- app/api/core/src/index.ts
+++ app/api/core/src/index.ts
@@ -4,7 +4,7 @@ import { ExpressFramework } from '@h4ad/serverless-adapter/lib/frameworks/expres
 import { DefaultHandler } from '@h4ad/serverless-adapter/lib/handlers/default';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/lib/adapters/aws';
-import { app } from './app';
+import {app} from './app';
 
 export const handler = ServerlessAdapter.new(app)
   .setFramework(new ExpressFramework())
