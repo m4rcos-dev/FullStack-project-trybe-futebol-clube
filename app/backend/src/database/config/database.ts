3e09d69a15dfd25559a589931ea19b01bb942751
m4rcos-dev <dev.marcospereira@gmail.com>
2023-03-21 21:26:32 -0300

    :sparkles: feat: edit database config and dockerfile

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
